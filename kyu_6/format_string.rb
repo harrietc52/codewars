d6bb1ece57965f972a2cb5aa4acb02687256d66b
Harriet Craven <harrietcraven@Harriets-MBP.home>
2015-08-23 16:01:09 +0100

    Completed Format a string of names

---
+++ kyu_6/format_string.rb
@@ -0,0 +1,9 @@
+def list(names)
+  arr = names.map { |hash| hash[:name] }.map { |i|  i.to_s }
+  return "" if arr.nil?
+  return arr[0] if arr.length == 1
+  return arr[0..-2].join(', ') + " and " + arr[-1] if arr.length > 1
+end
+
+# p list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+
